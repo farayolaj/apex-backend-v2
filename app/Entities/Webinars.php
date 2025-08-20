ba5ac077ad678655bd4a0f15b2d35276d2d698e5
Joshua Farayola <farayolajoshua@gmail.com>
2025-08-20 19:45:44 +0100

    feat: Add Webinars entity and migration
    Implement the Webinars entity for managing webinar data and create the corresponding migration for the webinars table.

---
+++ app/Entities/Webinars.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entities;
+
+use App\Models\Crud;
+
+/**
+ * This class queries those who have paid for both RuS and SuS
+ */
+class Webinars extends Crud
+{
+    protected static $tablename = 'webinars';
+
+    static $apiSelectClause = [
+        "course_id",
+        "room_id",
+        "title",
+        "description",
+        "scheduled_for",
+        "updated_at",
+        "created_at"
+    ];
+
+    public function list(int $courseId)
+    {
+        $query = "SELECT " . implode(", ", self::$apiSelectClause) . " FROM webinars WHERE course_id = $courseId ORDER BY scheduled_for DESC";
+        $res = $this->db->query($query);
+        return $res->getResultArray();
+    }
+
+    public function getDetails(int $webinarId)
+    {
+        $query = "SELECT " . implode(", ", self::$apiSelectClause) . " FROM webinars WHERE id = $webinarId";
+        $res = $this->db->query($query);
+        return $res->getRowArray();
+    }
+
+    public function create(array $data): int
+    {
+        $escapedValues = array_map(function ($value) {
+            return "'" . $this->db->escapeString($value) . "'";
+        }, array_values($data));
+        $query = "INSERT INTO webinars (" . implode(", ", array_keys($data)) . ") VALUES (" . implode(", ", $escapedValues) . ")";
+        $this->db->query($query);
+        return $this->db->insertID();
+    }
+
+    public function updateWebinar(int $webinarId, array $data): bool
+    {
+        $setClause = [];
+        foreach ($data as $key => $value) {
+            $setClause[] = "$key = " . "'" . $this->db->escapeString($value) . "'";
+        }
+        $query = "UPDATE webinars SET " . implode(", ", $setClause) . " WHERE id = $webinarId";
+        $this->db->query($query);
+        return $this->db->affectedRows() > 0;
+    }
+}
