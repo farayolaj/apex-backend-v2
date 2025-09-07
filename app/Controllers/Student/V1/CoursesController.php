6de8ee116067564aa06fc45a502b30c520ddfe61
Joshua Farayola <farayolajoshua@gmail.com>
2025-09-07 21:31:47 +0100

    feat: add course_guide_id to examination_courses entity and update result manager to include course guide URLs

--- app/Controllers/Student/V1/CoursesController.php
+++ app/Controllers/Student/V1/CoursesController.php
@@ -3,15 +3,26 @@
 namespace App\Controllers\Student\V1;
 
 use App\Controllers\BaseController;
+use App\Entities\Courses;
 use App\Libraries\ApiResponse;
 use App\Libraries\EntityLoader;
+use App\Services\GoogleDriveStorageService;
 
 class CoursesController extends BaseController
 {
+    private Courses $courses;
+
+    public function __construct()
+    {
+        $this->courses = EntityLoader::loadClass(null, 'courses');
+    }
+
     public function courseDetails($id)
     {
-        EntityLoader::loadClass($this, 'courses');
         $result = $this->courses->getDetails($id);
+        $result['course_guide'] = GoogleDriveStorageService::getPublicUrl(
+            $result['course_guide_id']
+        );
         return ApiResponse::success('success', $result);
     }
 }
--- app/Entities/Examination_courses.php
+++ app/Entities/Examination_courses.php
@@ -50,6 +50,7 @@ class Examination_courses extends Crud
 			c.course_id, c.session_id as session, 
 			MAX(b.code) as code, 
 			MAX(b.title) as title,
+            MAX(b.course_guide_id) as course_guide_id,
     		MAX(a.course_unit) as course_unit, 
     		COUNT(distinct a.student_id) as enrollment,
     		CASE
