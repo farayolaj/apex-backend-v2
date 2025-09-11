d0584c6eb85e3f3fd48cda1d05985339e9c8a046
Joshua Farayola <farayolajoshua@gmail.com>
2025-09-11 18:29:51 +0100

    feat: include session_id in tour settings response
    Add session_id to the response of getTourSettings method
    to provide complete tour configuration details.

--- app/Controllers/Student/V1/TourController.php
+++ app/Controllers/Student/V1/TourController.php
@@ -10,9 +10,11 @@ class TourController extends BaseController
     public function getTourSettings()
     {
         $tourCourseId = get_setting('tour_course_id');
+        $tourSessionId = get_setting('tour_session_id');
 
         return ApiResponse::success('success', [
             'course_id' => $tourCourseId,
+            'session_id' => $tourSessionId
         ]);
     }
 }
